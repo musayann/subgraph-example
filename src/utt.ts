7e6a4d20689563673ee59ead26fe26ba0c051f0e
musayann <musayann@octangrp.com>
2022-06-16 12:05:02 +0200

    feat: add utt abi and events

--- src/utt.ts
+++ src/utt.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
   UTT,
   AddConnection,
@@ -13,33 +13,30 @@ import {
   RewardPreviousEndorserLevel1,
   RewardPreviousEndorserLevel2,
   Transfer,
-  Unpaused
-} from "../generated/UTT/UTT"
-import { ExampleEntity } from "../generated/schema"
+  Unpaused,
+} from "../generated/UTT/UTT";
+import { ConnectionEntity, EndorseEntity } from "../generated/schema";
 
 export function handleAddConnection(event: AddConnection): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = ConnectionEntity.load(event.transaction.from.toHex());
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    entity = new ConnectionEntity(event.transaction.from.toHex());
 
     // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity.count = BigInt.fromI32(0);
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
   // Entity fields can be set based on event parameters
-  entity._user = event.params._user
-  entity._connectedTypeId = event.params._connectedTypeId
+  entity._user = event.params._user;
+  entity._connectedTypeId = event.params._connectedTypeId;
 
   // Entities can be written to the store with `.save()`
-  entity.save()
+  entity.save();
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
@@ -88,13 +85,40 @@ export function handleChainlinkFulfilled(event: ChainlinkFulfilled): void {}
 
 export function handleChainlinkRequested(event: ChainlinkRequested): void {}
 
-export function handleEndorse(event: Endorse): void {}
+export function handleEndorse(event: Endorse): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = EndorseEntity.load(event.transaction.from.toHex());
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new EndorseEntity(event.transaction.from.toHex());
+
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0);
+  }
+
+  // Entity fields can be set based on event parameters
+  entity._from = event.params._from;
+  entity._to = event.params._to;
+  entity._value = event.params._value;
+  entity._transactionId = event.params._transactionId;
+
+  // Entities can be written to the store with `.save()`
+  entity.save();
+}
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
 export function handlePaused(event: Paused): void {}
 
-export function handleRemoveConnection(event: RemoveConnection): void {}
+export function handleRemoveConnection(event: RemoveConnection): void {
+  // let entity = ConnectionEntity.load(event.transaction.from.toHex());
+  // if (entity) {
+  //   entity.delete();
+  // }
+}
 
 export function handleRewardPreviousEndorserLevel1(
   event: RewardPreviousEndorserLevel1
